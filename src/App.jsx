4fa4ac78e8a4e502d2de814c17934f94c1118d0a
Thegen Jackson <thegen.jackson@gmail.com>
2024-10-23 12:06:35 +1000

    23102024
    Created blank React app with Vite, installed Tailwind CSS and Daisy UI

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+export default function App() {
+  return (
+    
+    <div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+
+    <button class="btn">Button</button>
+
+    </div>
+  )
+}
